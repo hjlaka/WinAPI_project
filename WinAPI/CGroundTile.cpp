9dfcedb7a89dc7430ecfc8cbf87bd11b624a0105
HeejinLee <windamongtree@gmail.com>
2022-11-07 16:42:19 +0900

    feat: 임시 땅 충돌 추가

--- WinAPI/CGroundTile.cpp
+++ WinAPI/CGroundTile.cpp
@@ -42,11 +42,30 @@ void CGroundTile::Release()
 void CGroundTile::OnCollisionEnter(CCollider* pOther)
 {
 	// 땅타일과 충돌했을 경우 처리
+
+	
 }
 
 void CGroundTile::OnCollisionStay(CCollider* pOther)
 {
 	// 땅타일과 충돌했을 경우 처리
+
+	// 오브젝트와 충돌했다면 해당 오브젝트를 땅 타일 위로 올려준다.
+	//CGameObject* pOtherObj = pOther->GetOwner();
+	//// 땅 타일 위 
+	//float fGroundPosY = GetPos().y;
+	//float fGroundSclaeY = GetScale().y;
+	//float fObjScaleY = pOtherObj->GetScale().y;
+
+	//float fOnGroundY = fGroundPosY - (fObjScaleY / 2);
+	//Vector vecOnGround = Vector(pOtherObj->GetPos().x, fOnGroundY);
+	//pOtherObj->SetPos(vecOnGround);
+	
+	
+	// 중력 가속도의 크기와 똑같이 방향만 정 반대로 오브젝트를 밀어올려준다.
+	pOther->GetOwner()->SetPos(pOther->GetOwner()->GetPos() + Vector(0.f, -1.f) * 26.f * DT);
+	// 닿은 개수 만큼 속력이 달라진다.
+	
 }
 
 void CGroundTile::OnCollisionExit(CCollider* pOther)
